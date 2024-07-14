3409725dcc0e834de92fa673e97dc47720e08b77
gabrieloyarzo <gabrieloyarzo2015@gmail.com>
2024-07-14 14:31:44 -0400

    added required param to textfield in bulk-create

--- app/users/bulk-create/page.js
+++ app/users/bulk-create/page.js
@@ -8,109 +8,116 @@ import Navbar from "../../../components/Navbar";
 import "./page.css";
 
 export default function BulkCreateUsers() {
-  const [users, setUsers] = useState([]);
-  const [number, setNumber] = useState(1);
+    const [users, setUsers] = useState([]);
+    const [number, setNumber] = useState(1);
 
-  const [message, setMessage] = useState("");
-  const [openSnack, setOpenSnack] = useState(false);
+    const [message, setMessage] = useState("");
+    const [openSnack, setOpenSnack] = useState(false);
 
-  useEffect(() => {
-    setUsers(Array.from({ length: number }, () => ({})));
-  }, [number]);
+    useEffect(() => {
+        setUsers(Array.from({ length: number }, () => ({})));
+    }, [number]);
 
-  const handleChange = (e, index) => {
-    setUsers(
-      users.map((user, i) =>
-        i === index ? { ...user, [e.target.name]: e.target.value } : user
-      )
+    const handleChange = (e, index) => {
+        setUsers(
+            users.map((user, i) =>
+                i === index
+                    ? { ...user, [e.target.name]: e.target.value }
+                    : user
+            )
+        );
+    };
+
+    const createUsers = async () => {
+        const token = localStorage.getItem("token");
+        const response = await AuthService.bulkCreateUsers(token, users);
+        setMessage(response);
+        setOpenSnack(true);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        createUsers();
+    };
+
+    return (
+        <Container>
+            <SimpleSnackbar
+                message={message}
+                openSnack={openSnack}
+                closeSnack={() => {
+                    setOpenSnack(!openSnack);
+                }}
+            />
+            <Navbar />
+            <h1>Crear usuarios</h1>
+            <input
+                type="number"
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
+            />
+            {users.map((user, index) => (
+                <Stack key={index} direction="row" spacing={2} marginBottom={2}>
+                    <TextField
+                        className="form-input"
+                        label="Nombre"
+                        name="name"
+                        value={user?.name || ""}
+                        placeholder="juan"
+                        size="small"
+                        InputLabelProps={{ shrink: true }}
+                        onChange={(e) => handleChange(e, index)}
+                        required
+                    />
+                    <TextField
+                        className="form-input"
+                        label="Email"
+                        name="email"
+                        value={user?.email || ""}
+                        placeholder="juan@gmail.com"
+                        size="small"
+                        InputLabelProps={{ shrink: true }}
+                        onChange={(e) => handleChange(e, index)}
+                        required
+                    />
+                    <TextField
+                        className="form-input"
+                        type="password"
+                        name="password"
+                        value={user?.password || ""}
+                        label="Contraseña"
+                        placeholder="******"
+                        size="small"
+                        InputLabelProps={{ shrink: true }}
+                        onChange={(e) => handleChange(e, index)}
+                        required
+                    />
+                    <TextField
+                        className="form-input"
+                        type="password"
+                        name="password_second"
+                        value={user?.password_second || ""}
+                        label="Confirmar contraseña"
+                        placeholder="******"
+                        size="small"
+                        InputLabelProps={{ shrink: true }}
+                        onChange={(e) => handleChange(e, index)}
+                        required
+                    />
+                    <TextField
+                        className="form-input"
+                        label="Celular"
+                        name="cellphone"
+                        value={user?.cellphone || ""}
+                        placeholder="123456789"
+                        size="small"
+                        InputLabelProps={{ shrink: true }}
+                        onChange={(e) => handleChange(e, index)}
+                        required
+                    />
+                </Stack>
+            ))}
+            {number > 0 && <Button onClick={handleSubmit}>Crear</Button>}
+        </Container>
     );
-  };
-
-  const createUsers = async () => {
-    const token = localStorage.getItem("token");
-    const response = await AuthService.bulkCreateUsers(token, users);
-    setMessage(response);
-    setOpenSnack(true);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    createUsers();
-  };
-
-  return (
-    <Container>
-      <SimpleSnackbar
-        message={message}
-        openSnack={openSnack}
-        closeSnack={() => {
-          setOpenSnack(!openSnack);
-        }}
-      />
-      <Navbar />
-      <h1>Crear usuarios</h1>
-      <input
-        type="number"
-        value={number}
-        onChange={(e) => setNumber(e.target.value)}
-      />
-      {users.map((user, index) => (
-        <Stack key={index} direction="row" spacing={2} marginBottom={2}>
-          <TextField
-            className="form-input"
-            label="Nombre"
-            name="name"
-            value={user?.name || ""}
-            placeholder="juan"
-            size="small"
-            InputLabelProps={{ shrink: true }}
-            onChange={(e) => handleChange(e, index)}
-          />
-          <TextField
-            className="form-input"
-            label="Email"
-            name="email"
-            value={user?.email || ""}
-            placeholder="juan@gmail.com"
-            size="small"
-            InputLabelProps={{ shrink: true }}
-            onChange={(e) => handleChange(e, index)}
-          />
-          <TextField
-            className="form-input"
-            type="password"
-            name="password"
-            value={user?.password || ""}
-            label="Contraseña"
-            placeholder="******"
-            size="small"
-            InputLabelProps={{ shrink: true }}
-            onChange={(e) => handleChange(e, index)}
-          />
-          <TextField
-            className="form-input"
-            type="password"
-            name="password_second"
-            value={user?.password_second || ""}
-            label="Confirmar contraseña"
-            placeholder="******"
-            size="small"
-            InputLabelProps={{ shrink: true }}
-            onChange={(e) => handleChange(e, index)}
-          />
-          <TextField
-            className="form-input"
-            label="Celular"
-            name="cellphone"
-            value={user?.cellphone || ""}
-            placeholder="123456789"
-            size="small"
-            InputLabelProps={{ shrink: true }}
-            onChange={(e) => handleChange(e, index)}
-          />
-        </Stack>
-      ))}
-      {number > 0 && <Button onClick={handleSubmit}>Crear</Button>}
-    </Container>
-  );
 }
