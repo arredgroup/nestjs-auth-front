c9c333675c8b688f8da7837c441682e70a972817
Nico-MG <nico.jeremiah.guajardo@gmail.com>
2024-07-15 00:25:12 -0400

    fix endpoints

--- app/users/page.js
+++ app/users/page.js
@@ -1,12 +1,12 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import {
-    Container,
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableRow,
+	Container,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableRow,
 } from "@mui/material";
 import IconButton from "@mui/material/IconButton";
 import { Edit } from "@mui/icons-material";
@@ -18,81 +18,77 @@ import Link from "next/link";
 import "./page.css";
 
 export default function Users() {
-    const router = useRouter();
-    const [users, setUsers] = useState([]);
+	const router = useRouter();
+	const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-        const user = JSON.parse(localStorage.getItem("user"));
-        if (!user) {
-            router.push("/login");
-        }
-        if (user?.roles?.includes("admin")) {
-            getAllUsers();
-        }
-        if (user?.roles?.includes("user")) {
-            getUser(user.id);
-        }
-    }, []);
+	useEffect(() => {
+		const user = JSON.parse(localStorage.getItem("user"));
+		if (!user) {
+			router.push("/login");
+		}
+		if (user?.roles?.includes("admin")) {
+			getAllUsers();
+		}
+		if (user?.roles?.includes("user")) {
+			getUser(user.id);
+		}
+	}, []);
 
-    const getAllUsers = async () => {
-        const token = localStorage.getItem("token");
-        const data = await AuthService.getUsers(token);
-        setUsers(data.message);
-    };
+	const getAllUsers = async () => {
+		const token = localStorage.getItem("token");
+		const data = await AuthService.getUsers(token);
+		setUsers(data.message);
+	};
 
-    const getUser = async (id) => {
-        const token = localStorage.getItem("token");
-        const data = await AuthService.getUserById(id, token);
-        setUsers([data.message]);
-    };
+	const getUser = async (id) => {
+		const token = localStorage.getItem("token");
+		const data = await AuthService.getUserById(id, token);
+		setUsers([data.message]);
+	};
 
-    const handleEdit = (user) => {
-        router.push("/users/" + user.id + "/edit");
-    };
+	const handleEdit = (user) => {
+		router.push("/users/" + user.id + "/edit");
+	};
 
-    return (
-        <Container>
-            <Navbar />
-            <h1>Users</h1>
-            <Link href="/users/find" >
-                Filtrar usuarios
-            </Link>
-            <br />
-            <Link href="/users/bulk-create" color="primary">
-                Crear usuarios
-            </Link>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Nombre</TableCell>
-                        <TableCell>Email</TableCell>
-                        <TableCell>Estado</TableCell>
-                        <TableCell>Última Sesión</TableCell>
-                        <TableCell>Acciones</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {users.map((user) => (
-                        <TableRow key={user}>
-                            <TableCell>{user.name}</TableCell>
-                            <TableCell>{user.email}</TableCell>
-                            <TableCell>
-                                {user.status ? "ACTIVO" : "CERRADO"}
-                            </TableCell>
-                            <TableCell>TBD</TableCell>
-                            <TableCell>
-                                <IconButton
-                                    color="primary"
-                                    aria-label={"Editar usuario " + user.name}
-                                    onClick={() => handleEdit(user)}
-                                >
-                                    <Edit />
-                                </IconButton>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </Container>
-    );
+	return (
+		<Container>
+			<Navbar />
+			<h1>Users</h1>
+			<Link href="/users/findUsers">Filtrar usuarios</Link>
+			<br />
+			<Link href="/users/bulkCreate" color="primary">
+				Crear usuarios
+			</Link>
+			<Table>
+				<TableHead>
+					<TableRow>
+						<TableCell>Nombre</TableCell>
+						<TableCell>Email</TableCell>
+						<TableCell>Estado</TableCell>
+						<TableCell>Última Sesión</TableCell>
+						<TableCell>Acciones</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{users.map((user) => (
+						<TableRow key={user}>
+							<TableCell>{user.name}</TableCell>
+							<TableCell>{user.email}</TableCell>
+							<TableCell>{user.status ? "ACTIVO" : "CERRADO"}</TableCell>
+							<TableCell>TBD</TableCell>
+							<TableCell>
+								<IconButton
+									color="primary"
+									aria-label={"Editar usuario " + user.name}
+									onClick={() => handleEdit(user)}
+								>
+									<Edit />
+								</IconButton>
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+		</Container>
+	);
 }
