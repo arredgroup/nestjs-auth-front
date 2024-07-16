1d26853d003dc119761e4923d2bf018b3f0ddaa9
Katia Arevalo <katiaalejandra.arevalo@alumnos.ulagos.cl>
2024-07-16 07:35:16 -0400

    Trabajo final: Electivo Frontend.

--- app/users/page.js
+++ app/users/page.js
@@ -1,50 +1,198 @@
-"use client"
-import React, {useEffect, useState} from 'react';
-import {Container, Table, TableBody, TableCell, TableHead, TableRow} from "@mui/material";
-import IconButton from '@mui/material/IconButton';
-import {Edit} from "@mui/icons-material";
-
+"use client";
+import React, { useEffect, useState } from "react";
+import {Container, Table, TableBody, TableCell, TableHead, TableRow, Button, Stack, TextField, Grid, MenuItem, Switch, FormControlLabel, Box} from "@mui/material";
 import AuthService from "../../services/AuthService";
-import { useRouter } from 'next/navigation';
-import Navbar from '../../components/Navbar';
+import { useRouter } from "next/navigation";
+import Navbar from "../../components/Navbar";
 
-export default function Users(){
-
+export default function FindUsers() {
     const router = useRouter();
     const [users, setUsers] = useState([]);
+    const [query, setQuery] = useState({
+        name: '',
+        login_after_date: '',
+        login_before_date: '',
+        status: '', 
+    });
+    const [searchTerm, setSearchTerm] = useState('');
+    const [dateFilter, setDateFilter] = useState('');
+    const [selectedDate, setSelectedDate] = useState('');
 
     useEffect(() => {
-        const user = JSON.parse(localStorage.getItem('user'));
-        if(!user){
-            router.push('/login');
+        const user = JSON.parse(localStorage.getItem("user"));
+        if (!user) {
+            router.push("/login");
         }
-        if(user?.roles?.includes('admin')){
-            getAllUsers();
+        if (user?.roles?.includes("admin")) {
+            getFilteredUsers();
         }
-        if(user?.roles?.includes('user')){
-            getUser(user.id);
+    }, [query]);
+
+    const getFilteredUsers = async () => {
+        try {
+            const token = localStorage.getItem("token");
+            const data = await AuthService.findUsers(token, query);
+            setUsers(data);
+        } catch (error) {
+            console.error("Error al obtener usuarios filtrados:", error);
         }
-    }, []);
+    };
 
-    const getAllUsers = async () => {
-        const data = await AuthService.getUsers();
-        setUsers(data);
-    }
+    const handleSearchTermChange = (event) => {
+        setSearchTerm(event.target.value);
+        filterUsers(event.target.value, query.login_after_date, query.login_before_date, query.status);
+    };
 
-    const getUser = async (id) => {
-        const token = localStorage.getItem('token');
-        const data = await AuthService.getUserById(id, token);
-        setUsers([data]);
-    }
+    const handleClearFilters = () => {
+        setQuery({
+            name: '',
+            login_after_date: '',
+            login_before_date: '',
+            status: '', 
+        });
+        setDateFilter('');
+        setSelectedDate('');
+        filterUsers('', '', '', '');
+    };
 
-    const handleEdit = (user) => {
-        router.push('/users/' + user.id + '/edit');
-    }
+    const handleDateFilterChange = (event) => {
+        setDateFilter(event.target.value);
+        setSelectedDate('');
+        filterUsers(searchTerm, query.login_after_date, query.login_before_date, query.status);
+    };
+
+    const handleSelectedDateChange = (event) => {
+        setSelectedDate(event.target.value);
+        filterUsers(searchTerm, query.login_after_date, query.login_before_date, query.status);
+    };
+
+    const handleDateChange = (event, id) => {
+        setQuery({
+            ...query,
+            [id]: event.target.value
+        });
+        filterUsers(searchTerm, id === 'login_after_date' ? event.target.value : query.login_after_date, id === 'login_before_date' ? event.target.value : query.login_before_date, query.status);
+    };
+
+    const handleStatusChange = (event) => {
+        setQuery({
+            ...query,
+            status: event.target.value
+        });
+        filterUsers(searchTerm, query.login_after_date, query.login_before_date, event.target.value);
+    };
+
+    const filterUsers = (searchTerm, login_after_date, login_before_date, status) => {
+        let filteredUsers = users;
+
+        if (searchTerm) {
+            const lowerCaseSearch = searchTerm.toLowerCase();
+            filteredUsers = filteredUsers.filter(user =>
+                user.name.toLowerCase().includes(lowerCaseSearch)
+            );
+        }
+
+        if (login_after_date) {
+            filteredUsers = filteredUsers.filter(user =>
+                new Date(user.createdAt) >= new Date(login_after_date)
+            );
+        }
+
+        if (login_before_date) {
+            filteredUsers = filteredUsers.filter(user =>
+                new Date(user.createdAt) <= new Date(login_before_date)
+            );
+        }
+
+        if (status) {
+            filteredUsers = filteredUsers.filter(user =>
+                user.status === (status === 'active' ? true : false)
+            );
+        }
+
+        setUsers(filteredUsers);
+    };
 
     return (
         <Container>
             <Navbar />
-            <h1>Users</h1>
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Box>
+                    <h1>Lista de usuarios</h1>
+                </Box>
+                <Box>
+                    <Button
+                        variant="contained"
+                        onClick={() => {
+                            router.push('/users/bulkcreate');
+                        }}
+                    >
+                        Crear usuarios
+                    </Button>
+                </Box>
+            </Box>
+            <h3> Filtros de usuarios </h3>
+            <Stack direction="row" spacing={4} alignItems="center">
+                <Grid container spacing={2} alignItems="center">
+                    <Grid item xs={3.5} style={{ marginLeft: '-1px' }}>
+                        <TextField
+                        label="Buscar por nombre de usuario"
+                        variant="outlined"
+                        value={searchTerm}
+                        onChange={handleSearchTermChange}
+                        fullWidth
+                        margin="none"  
+                        />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <TextField
+                            select
+                            label="Filtrar por fecha"
+                            value={dateFilter}
+                            onChange={handleDateFilterChange}
+                            variant="outlined"
+                            fullWidth
+                            margin="none"
+                        >
+                            <MenuItem value="">Todos</MenuItem>
+                            <MenuItem value="before">Antes de</MenuItem>
+                            <MenuItem value="after">Después de</MenuItem>
+                        </TextField>
+                    </Grid>
+                    {dateFilter && (
+                        <Grid item xs={3}>
+                            <TextField
+                                type="date"
+                                value={selectedDate}
+                                onChange={handleSelectedDateChange}
+                                variant="outlined"
+                                fullWidth
+                                margin="none"
+                            />
+                        </Grid>
+                    )}
+                     <Grid item xs={2}>
+                        <FormControlLabel
+                            control={
+                                <Switch
+                                    id="active"
+                                    checked={query.active}
+                                    onChange={(e) =>
+                                        setQuery({ ...query, active: e.target.checked })
+                                    }
+                                />
+                            }
+                            label="Estado"
+                            labelPlacement="top"
+                        />
+                    </Grid>
+                </Grid>
+                <Stack direction="row" spacing={5} sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Button onClick={handleClearFilters} sx={{ whiteSpace: 'nowrap' }}>
+                         Borrar filtros
+                    </Button>
+                </Stack>
+            </Stack>
             <Table>
                 <TableHead>
                     <TableRow>
@@ -52,27 +200,19 @@ export default function Users(){
                         <TableCell>Email</TableCell>
                         <TableCell>Estado</TableCell>
                         <TableCell>Última Sesión</TableCell>
-                        <TableCell>Acciones</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {
-                        users.map((user) => (
-                            <TableRow key={user}>
-                                <TableCell>{user.name}</TableCell>
-                                <TableCell>{user.email}</TableCell>
-                                <TableCell>{user.status? 'ACTIVO' : 'CERRADO'}</TableCell>
-                                <TableCell>TBD</TableCell>
-                                <TableCell>
-                                    <IconButton color="primary" aria-label={"Editar usuario " + user.name} onClick={() => handleEdit(user)}>
-                                        <Edit />
-                                    </IconButton>
-                                </TableCell>
-                            </TableRow>
-                        ))
-                    }
+                    {users.map((user, index) => (
+                        <TableRow key={index}>
+                            <TableCell>{user.name}</TableCell>
+                            <TableCell>{user.email}</TableCell>
+                            <TableCell>{user.status ? "ACTIVO" : "CERRADO"}</TableCell>
+                            <TableCell>{user.createdAt}</TableCell>
+                        </TableRow>
+                    ))}
                 </TableBody>
             </Table>
         </Container>
-    )
+    );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './Navbar.css';
 import Button from '@mui/material/Button';
 import AuthService from '@/services/AuthService';
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 
 const Navbar = () => {
     const router = useRouter();
-    const [user, setUser] = useState({name:""});
+    const [user, setUser] = useState({ name: "" });
 
     useEffect(() => {
         setUser(JSON.parse(localStorage.getItem('user')));
@@ -15,7 +15,7 @@ const Navbar = () => {
     const handleLogout = async () => {
         const token = localStorage.getItem('token');
         const result = await AuthService.logOut(token);
-        if(result){
+        if (result) {
             router.push('/login');
         }
     }
@@ -23,10 +23,11 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div className="navbar-item">
-                {user?.name}
+                ¡Nos alegra verte de vuelta, {user.name}!
             </div>
             <div className="navbar-item">
-                <Button onClick={handleLogout}>
+                <Button variant="contained" onClick={handleLogout}>
+                    
                     Logout
                 </Button>
             </div>
